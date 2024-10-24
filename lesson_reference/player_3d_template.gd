6bdf22013474d0479f5acada3b675b19d8a6a040
CyrusVillaruz <cyrusvillaruz1@gmail.com>
2024-10-24 08:12:20 -0700

    Modifies toggle for mouse capture

--- lesson_reference/player_3d_template.gd
+++ lesson_reference/player_3d_template.gd
@@ -57,9 +57,9 @@ func _ready() -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("show_mouse_cursor"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	elif event.is_action_pressed("left_click"):
+	elif event.is_action_released("show_mouse_cursor"):
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
